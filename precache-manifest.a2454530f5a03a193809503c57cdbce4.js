d35ff5207cb705d2d070bbf8316e439934cacb8b
stockdepot <itcwangfeng@hotmail.com>
2021-06-23 21:20:04 -0500

    First commit

---
+++ precache-manifest.a2454530f5a03a193809503c57cdbce4.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95f4e44da413b17bf2ed87c8e3a8ed3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e91a82b4792b213ae95b",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e252df86981e263bbb7b",
+    "url": "/static/js/2.c99a354b.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/static/js/2.c99a354b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e91a82b4792b213ae95b",
+    "url": "/static/js/main.6e8f857d.chunk.js"
+  },
+  {
+    "revision": "f66f7366363de71fca0d",
+    "url": "/static/js/runtime-main.ff2a2ca0.js"
+  },
+  {
+    "revision": "0f16b2c727eced3ea5d7aaa0e2a72aa0",
+    "url": "/static/media/Feng.0f16b2c7.png"
+  },
+  {
+    "revision": "0db058eeb751eb6c6b3ca9cb0a3ac6b5",
+    "url": "/static/media/Halliburton.0db058ee.png"
+  },
+  {
+    "revision": "872665a9e925e4835a17b2916e7943a0",
+    "url": "/static/media/StockHub.872665a9.png"
+  },
+  {
+    "revision": "8d815caa295f66a955eacbe847466a31",
+    "url": "/static/media/Wuhan.8d815caa.png"
+  },
+  {
+    "revision": "c38ba5b5dbe84be802650c2f4ed8608d",
+    "url": "/static/media/WuhanUniversity.c38ba5b5.png"
+  },
+  {
+    "revision": "17817b53c79cec098e05f054c0c7a5db",
+    "url": "/static/media/WuhanUniversity2.17817b53.png"
+  },
+  {
+    "revision": "3c9d6ae7f2467ee0e0630a0111c8e7e0",
+    "url": "/static/media/chevron.3c9d6ae7.png"
+  },
+  {
+    "revision": "d7f9c11e43f1917fe19a908cd0af9276",
+    "url": "/static/media/degree.d7f9c11e.png"
+  },
+  {
+    "revision": "9c9a5c3b2cd25a7d7d1aeaca48aa9066",
+    "url": "/static/media/hobby.9c9a5c3b.png"
+  },
+  {
+    "revision": "302718b74c367f863c81c5f85833d44e",
+    "url": "/static/media/home.302718b7.svg"
+  },
+  {
+    "revision": "e9892c99840ea64cfe05e679dd74da1d",
+    "url": "/static/media/microsoft.e9892c99.png"
+  },
+  {
+    "revision": "b720d57e476192fbbe6e59378201b34a",
+    "url": "/static/media/programming.b720d57e.png"
+  },
+  {
+    "revision": "6230ecdbda845552db88522523857cd9",
+    "url": "/static/media/skills.6230ecdb.png"
+  },
+  {
+    "revision": "a3ceb2f8ec3556625fb06e1b1c73a88f",
+    "url": "/static/media/wellflag.a3ceb2f8.png"
+  }
+]);
